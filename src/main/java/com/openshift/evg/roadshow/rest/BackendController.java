ca2a936b7a44bdf774d5f7dfc9bc9541a2247b4f
Jorge Morales Pou <jorgemoralespou@gmail.com>
2016-09-29 23:18:40 +0200

    Added BackendController

---
+++ src/main/java/com/openshift/evg/roadshow/rest/BackendController.java
@@ -0,0 +1,25 @@
+package com.openshift.evg.roadshow.rest;
+
+import com.openshift.evg.roadshow.rest.gateway.model.Backend;
+import com.openshift.evg.roadshow.rest.gateway.model.Coordinates;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+/**
+ * Provides information about this backend
+ *
+ * Created by jmorales on 26/09/16.
+ */
+@Path("/info")
+public class BackendController{
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Backend get() {
+        return new Backend("mlbparks","MLB Parks", "mlbparks:8080", new Coordinates("39.82", "-98.57"), 5);
+    }
+}
